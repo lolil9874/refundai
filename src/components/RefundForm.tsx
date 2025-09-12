85dd3b4328178891315278ee61246ab40d076d79
[dyad] <helwanllm@gmail.com>
2025-09-12 13:21:08 -0700

    Reverted all changes back to version 809ef0990a17824af612b3bf996dd973174f99e5

--- src/components/RefundForm.tsx
+++ src/components/RefundForm.tsx
@@ -160,7 +160,7 @@ export function RefundForm({
               <CardTitle>{t("refundForm.orderDetailsSectionTitle")}</CardTitle>
             </CardHeader>
             <CardContent className="space-y-6">
-              <ImageUpload isLoading={isUploading} />
+              <ImageUpload isLoading={isUploading} setFormValue={setValue} />
               <OrderDetailsForm />
               <IssueSelector />
               <DescriptionField />
