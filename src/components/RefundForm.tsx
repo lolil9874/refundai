2d57cebb1d0ac4fcb8defd93eea0f07a0f239930
[dyad] <helwanllm@gmail.com>
2025-09-13 16:02:40 -0700

    [dyad] Moving OCR feature to the top of the form - wrote 1 file(s)

--- src/components/RefundForm.tsx
+++ src/components/RefundForm.tsx
@@ -104,6 +104,12 @@ export function RefundForm({
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-8">
         <Card>
+          <CardContent className="pt-6">
+            <ImageUpload isLoading={isLoading} />
+          </CardContent>
+        </Card>
+
+        <Card>
           <CardHeader>
             <CardTitle>{t("refundForm.title")}</CardTitle>
             <CardDescription>{t("refundForm.subtitle")}</CardDescription>
@@ -127,7 +133,6 @@ export function RefundForm({
           </CardHeader>
           <CardContent className="space-y-8">
             <div className="space-y-6">
-              <ImageUpload isLoading={isLoading} />
               <OrderDetailsForm />
             </div>
             <div className="space-y-6">
